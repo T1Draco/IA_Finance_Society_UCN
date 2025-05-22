f57b4299d3320e4c8d7d4e18a1467655ec555c73
Martín Puebla <martin.puebla.rivera@gmail.com>
2025-05-22 12:44:30 -0400

    Ajustando modelo LSTM AAPL

--- Archivos Python y Data/3_Modelado_Precios_Futuros/models/LSTM/multivariable_lstm_model_AAPL.py
+++ Archivos Python y Data/3_Modelado_Precios_Futuros/models/LSTM/multivariable_lstm_model_AAPL.py
@@ -56,9 +56,9 @@ train_loader = DataLoader(TensorDataset(X_train, y_train), batch_size=batch_size
 class LSTMModel(nn.Module):
     def __init__(self, input_size):
         super().__init__()
-        self.lstm = nn.LSTM(input_size=input_size, hidden_size=128, num_layers=2, batch_first=True, dropout=0.2)
+        self.lstm = nn.LSTM(input_size=input_size, hidden_size=512, num_layers=2, batch_first=True, dropout=0.2)
         self.fc = nn.Sequential(
-            nn.Linear(128, 128),
+            nn.Linear(512, 128),
             nn.ReLU(),
             nn.Linear(128, 128),
             nn.ReLU(),
